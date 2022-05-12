686f7b7dacbab5492badcd1baf643c2decf9b7a6
Pekka Enberg <penberg@iki.fi>
2022-05-12 09:18:42 +0300

    Add Next.js API rewrites to ChiselStrike

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ module.exports = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/dev/:path*',
+      },
+    ]
+  },
 };
