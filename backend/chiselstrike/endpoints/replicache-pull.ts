b0f46b1d7986061ae5c3c885e8c7fcf567a67ff2
Pekka Enberg <penberg@iki.fi>
2022-07-19 10:53:08 +0300

    Simplify pull endpoint

--- backend/chiselstrike/endpoints/replicache-pull.ts
+++ backend/chiselstrike/endpoints/replicache-pull.ts
@@ -7,32 +7,30 @@ export default async function (req: Request): Promise<Response> {
     const url = new URL(req.url);
     const spaceID = url.searchParams.get("spaceID");
     const pull = await req.json();
-    let requestCookie = pull.cookie ?? 0;
+    const requestCookie = pull.cookie ?? 0;
     const clientID = pull.clientID;
-    const lastMutationID = (await Client.findOne({ clientID }))?.lastmutationid;
-    const responseCookie = (await Space.findOne({ spaceID }))?.version;
-    const resp = {
-        lastMutationID: lastMutationID ?? 0,
-        cookie: responseCookie ?? 0,
-        patch: [],
-    };
-    const entries = await Entry.findMany((entry) =>
-        entry.spaceid == spaceID && entry.version > requestCookie
-    );
-    for (const entry of entries) {
+    const patch = await Entry.cursor().filter(entry => {
+        return entry.spaceid == spaceID && entry.version > requestCookie;
+    }).map(entry => {
         if (entry.deleted) {
-            resp.patch.push({
+            return {
                 op: "del",
                 key: entry.key,
-            });
+            };
         } else {
             const value = JSON.parse(entry.value);
-            resp.patch.push({
+            return {
                 op: "put",
                 key: entry.key,
                 value,
-            });
+            };
         }
-    }
-    return responseFromJson(resp);
+    }).toArray();
+    const lastMutationID = (await Client.findOne({ clientID }))?.lastmutationid;
+    const responseCookie = (await Space.findOne({ spaceID }))?.version;
+    return {
+        lastMutationID: lastMutationID ?? 0,
+        cookie: responseCookie ?? 0,
+        patch,
+    };
 }
